4ee85e455f9a7fe475c77eb045bbb0d81f27786d
TaeWoo Kim <98134264+xodn246@users.noreply.github.com>
2025-12-30 20:44:26 +0900

    update
    player 헤더, cpp 주석처리

--- CH2TeamAssign/CH2TeamAssign/Player.cpp
+++ CH2TeamAssign/CH2TeamAssign/Player.cpp
@@ -1,45 +1,45 @@
 // 김태우_테스트용 플레이어 cpp
-#include <iostream>
-#include <string>
-#include "Player.h"
-
-using namespace std;
-
-void Player::RestoreHP(int restore) {
-    if (currentHealth < maxHealth)
-    {
-        cout << "체력 회복" << endl;
-        currentHealth += restore;
-
-        if(currentHealth > maxHealth) {
-            currentHealth = maxHealth;
-            cout << "현재체력을 최대체력과 맞게 보정" << endl;
-        }
-        cout << "현재 체력 : " << currentHealth << endl;
-    }
-    else 
-    {
-        cout << "체력이 가득차있습니다." << endl;
-    }
-}
-
-void Player::IncreaseATK(int bounsAtk) {
-    cout << "공격력 " << bounsAtk << "만큼 증가" << endl;
-    bonusAttack = bounsAtk;
-}
-
-void Player::TakeDamage(int damage) {
-    cout << damage << " 만큼 데미지 입음!!" << endl;
-    currentHealth -= damage;
-    
-    cout << "남은 체력 : " << currentHealth << endl;
-}
-
-void Player::EndBuff() {
-    cout << "전투 종료시 호출 : 버프 종료" << endl;
-    bonusAttack = 0;
-}
-
-void Player::ViewATK() {
-    cout << "현재 공격력 : " << attack + bonusAttack << endl;
-}
+//#include <iostream>
+//#include <string>
+//#include "Player.h"
+//
+//using namespace std;
+//
+//void Player::RestoreHP(int restore) {
+//    if (currentHealth < maxHealth)
+//    {
+//        cout << "체력 회복" << endl;
+//        currentHealth += restore;
+//
+//        if(currentHealth > maxHealth) {
+//            currentHealth = maxHealth;
+//            cout << "현재체력을 최대체력과 맞게 보정" << endl;
+//        }
+//        cout << "현재 체력 : " << currentHealth << endl;
+//    }
+//    else 
+//    {
+//        cout << "체력이 가득차있습니다." << endl;
+//    }
+//}
+//
+//void Player::IncreaseATK(int bounsAtk) {
+//    cout << "공격력 " << bounsAtk << "만큼 증가" << endl;
+//    bonusAttack = bounsAtk;
+//}
+//
+//void Player::TakeDamage(int damage) {
+//    cout << damage << " 만큼 데미지 입음!!" << endl;
+//    currentHealth -= damage;
+//    
+//    cout << "남은 체력 : " << currentHealth << endl;
+//}
+//
+//void Player::EndBuff() {
+//    cout << "전투 종료시 호출 : 버프 종료" << endl;
+//    bonusAttack = 0;
+//}
+//
+//void Player::ViewATK() {
+//    cout << "현재 공격력 : " << attack + bonusAttack << endl;
+//}
--- CH2TeamAssign/CH2TeamAssign/Player.h
+++ CH2TeamAssign/CH2TeamAssign/Player.h
@@ -3,20 +3,20 @@
 #ifndef Player_H_
 #define Player_H_
 
-class Player {
-private:
-    int maxHealth = 30;
-    int currentHealth = 30;
-    int attack = 10;
-    int bonusAttack = 0;
-
-public:
-    void RestoreHP(int restore);
-    void IncreaseATK(int increase);
-    void EndBuff();
-    void TakeDamage(int damage);
-    void ViewATK();
-};
+//class Player {
+//private:
+//    int maxHealth = 30;
+//    int currentHealth = 30;
+//    int attack = 10;
+//    int bonusAttack = 0;
+//
+//public:
+//    void RestoreHP(int restore);
+//    void IncreaseATK(int increase);
+//    void EndBuff();
+//    void TakeDamage(int damage);
+//    void ViewATK();
+//};
 
 #endif
 
