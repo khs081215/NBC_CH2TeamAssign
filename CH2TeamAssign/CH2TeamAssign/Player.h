dd6bfb5a6d6c037e7155af75e994eafd2f67a9e7
TaeWoo Kim <98134264+xodn246@users.noreply.github.com>
2025-12-31 10:48:47 +0900

    update
    player.cpp, player.h 초기화

--- CH2TeamAssign/CH2TeamAssign/Player.h
+++ CH2TeamAssign/CH2TeamAssign/Player.h
@@ -1,22 +1,13 @@
-// 김태우_테스트용 플레이어 클래스
 #pragma once
 #ifndef Player_H_
 #define Player_H_
 
-//class Player {
-//private:
-//    int maxHealth = 30;
-//    int currentHealth = 30;
-//    int attack = 10;
-//    int bonusAttack = 0;
-//
-//public:
-//    void RestoreHP(int restore);
-//    void IncreaseATK(int increase);
-//    void EndBuff();
-//    void TakeDamage(int damage);
-//    void ViewATK();
-//};
+// 여기서부터 작성
+
+
+
+
+
 
 #endif
 
